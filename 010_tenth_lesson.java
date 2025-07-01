dffc73c240a9b7f2c05884b975b20f5c15f3f159
Saxophilyman <86315790+Saxophilyman@users.noreply.github.com>
2025-07-02 00:07:36 +0400

    Update 010_tenth_lesson.java

--- 010_tenth_lesson.java
+++ 010_tenth_lesson.java
@@ -1,1 +1,19 @@
+В Java имеется возможность запрета переопределения методов в потомках, если в методе ставится ключевое слово final
 
+public class Y {
+//переопределяемый метод
+    public String methodOpen() {
+        return methodFinal();
+    }
+//непереопределяемый метод
+    public final String methodFinal() {
+        return "Базовая реализация";
+    }
+}
+
+public class Z extends Y {
+    @Override
+    public String methodOpen() {
+        return "Своя реализация в классе Z"; //стоит применять разумно
+    }
+}
